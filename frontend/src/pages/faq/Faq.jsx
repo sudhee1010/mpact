85b2be6eccb10f073ba5771b74940f02e36cca8d
unknown <gourinandahh75@gmail.com>
2025-12-19 11:16:33 +0530

    Faq

---
+++ frontend/src/pages/faq/Faq.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Faq.css"
+
+function Faq() {
+  return (
+    <div>
+      <section class="a">
+        <h1>FREQUENTLY ASKED QUESTIONS</h1>
+      </section>
+      <section>
+        <h2>PRODUCT INFORMATION</h2>
+      </section>
+    </div>
+  )
+}
+
+export default Faq
