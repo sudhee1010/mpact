3782e22b5935786e9cd02bd33012476056bacdd6
devapriya <devpriyascipy@gmail.com>
2025-12-29 13:45:17 +0530

    profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Routes, Route ,BrowserRouter} from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import Profile from "./pages/profile/Profile";
+import Prfle from "./pages/profile/Prfle";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Checkout from "./pages/Checkout";
@@ -18,6 +19,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Product />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/prfle" element={<Prfle />}/>
+
+
         </Routes>
       </div>
 
